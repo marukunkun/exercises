a5de3b562c5f1b4ce716b4ee40bf46c2ae2b9370
marukun <marukun.dev@gmail.com>
2024-10-04 10:23:28 +0300

    exercise from codeAcademy

---
+++ dogs_years_codeAcademy.js
@@ -0,0 +1,16 @@
+// my age
+let myAge = 36
+let earlyYears = 2
+
+// calculate the equivalent in dog's year
+earlyYears *= 10.5
+myAge -= 2
+laterYears = myAge
+laterYears *= 4
+
+let myAgeInDogYears = earlyYears + laterYears
+
+// declaring and assigning myName
+let myName = 'Marukun'.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old n dog years.`)
